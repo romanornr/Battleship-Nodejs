c391b0b41ce5a5af3aa2edf5e8bdb80c4b51fd35
romanornr <romanornr@gmail.com>
2016-03-18 01:06:13 +0100

    Vuejs logic for placing & previewing ships
    (Still need to make a mouse out event

--- vuejs/game.js
+++ vuejs/game.js
@@ -1,3 +1,5 @@
+Vue.config.debug = true;
+
 Vue.component('board', {
 	props:['columns', 'rows'],
 	template: '#board-template',
@@ -14,7 +16,116 @@ Vue.component('board', {
 				}
 				return element;
 			}
+	},
+
+	methods: {
+		placeTheShip: function(game){
+
+			console.log(game.currentTarget.getAttribute('data-coordination'));
+
+			if(this.$root.chosenShip == null || this.$root.chosenShip == 0) return;
+			var setCoordination = game.currentTarget.getAttribute('data-coordination');
+			var size = this.$root.chosenShip.size;
+			var hoveredTile = document.querySelectorAll('.title-hover');
+			var overlap = false; //check for placing colission 
+
+			for(var i = 0; i < size; i++){
+				if(this.$root.rotated){
+					if(parseInt(setCoordination.split(".").reverse().join(".")[0]) + size <=this.columns){
+						var e = document.querySelector('[data-coordination="'+(parseInt(setCoordination) + (i))+'"]');
+						if (e.className == 'placed-tile') overlap = true;
+					}else{
+						var e = document.querySelector('[data-coordination="' + (parseInt(setCoordination) - (i))+'"]'); 
+						if (e.className = 'placed-tile') overlap = true;
+						}
+					} else if(!this.$root.rotated){
+						if(document.querySelector('[data-coordination="' + (parseInt(setCoordination) + (i * 10)) + '"]') != null){
+							var e = document.querySelector('[data-coordination="'+ (parseInt(setCoordination) + (i * 10)) +'"]');
+							if(e.className = 'placed-tile') overlap=true;
+						}else{
+							var e = document.querySelector('[data-coordination="'+ (parseInt(setCoordination) - ((size - i) *10)) + '"]');
+							if(e.className == 'placed-tile') overlap= true; 
+						}
+					}
+					console.log(e.className);
+
+				}if(!overlap){
+					this.$root.chosenShip.amount--;
+					for (var i=0; i < hoveredTile.length; i++){
+						hoveredTile[i].className = 'placed-tile';
+					}
+				}
+			},
+changeStyle: function(el) {
+
+			if(this.$root.chosenShip == null || this.$root.chosenShip.amount == 0)
+				return;
+
+			var setCoordination = el.currentTarget.getAttribute('data-coordination');
+
+			var size = this.$root.chosenShip.size;
+
+			console.log(setCoordination + ' is the Coordination');
+				
+
+			for (var i = 0; i < size; i++) {
+				var e = document.querySelector('[data-coordination="'+ setCoordination + (i)+'"]');
+				console.log(e);
+
+				if(this.$root.rotated) {
+					if (parseInt(setCoordination.split("").reverse().join("")[0]) + size <= this.columns) {
+						var e = document.querySelector('[data-coordination="'+ (parseInt(setCoordination) + (i)) +'"]');
+						e.className = e.className == 'placed-tile' ? 'placed-tile' : 'tile-hover';
+					}
+					else {
+						var e = document.querySelector('[data-coordination="'+ (parseInt(setCoordination) - (i)) +'"]');
+						e.className = e.className == 'placed-tile' ? 'placed-tile' : 'tile-hover';
+					}
+				} else if (!this.$root.rotated) {
+					if (document.querySelector('[data-coordination="'+ (parseInt(setCoordination) + (i * 10)) +'"]') != null) {
+						var e = document.querySelector('[data-coordination="'+ (parseInt(setCoordination) + (i * 10)) +'"]');
+						e.className = e.className == 'placed-tile' ? 'placed-tile' : 'tile-hover';
+					}
+					else {
+						var e = document.querySelector('[data-coordination="'+ (parseInt(setCoordination) - ((size - i) * 10)) +'"]');
+						e.className = e.className == 'placed-tile' ? 'placed-tile' : 'tile-hover';
+					}
+				}
+
+			}
+
+		},
+
+		setDef: function(el) {
+			if(this.$root.chosenShip == null)
+				return;
+			var setCoordination = el.currentTarget.getAttribute('data-coordination');
+
+			var size = this.$root.chosenShip.size;
+
+			for (var i = 0; i < size; i++)
+				if(this.$root.rotated) {
+					if (parseInt(setCoordination.split("").reverse().join("")[0]) + size <= this.cols) {
+						var e = document.querySelector('[data-coordination="'+ (parseInt(setCoordination) + (i * 1)) +'"]');
+						e.className  = e.className == 'placed-tile' ? 'placed-tile' : 'tile';
+					}
+					else {
+						var e = document.querySelector('[data-coordination="'+ (parseInt(setCoordination) - ((i) * 1)) +'"]');
+						e.className  = e.className == 'placed-tile' ? 'placed-tile' : 'tile';
+					}
+				} else if (!this.$root.rotated) {
+					if (document.querySelector('[data-coordination="'+ (parseInt(setCoordination) + (i * 10)) +'"]') != null) {
+						var e = document.querySelector('[data-coordination="'+ (parseInt(setCoordination) + (i * 10)) +'"]');
+						e.className  = e.className == 'placed-tile' ? 'placed-tile' : 'tile';
+					}
+					else {
+						var e = document.querySelector('[data-coordination="'+ (parseInt(setCoordination) - ((size - i) * 10)) +'"]');
+						e.className  = e.className == 'placed-tile' ? 'placed-tile' : 'tile';
+					}
+				}
+		}
 	}
+
 });
 
 var vm = new Vue({
@@ -40,6 +151,21 @@ var vm = new Vue({
 	methods: {
 		setChosenShip: function(ship){
 			this.chosenShip = ship;
+			console.log(this.chosenShip = ship);
+		}
+	}, 
+
+	computed: {
+		ready: function(){
+			var ready = true;
+			this.ships.Foreach(function(element, index){
+				if(element.available > 0)
+					ready = false;
+			});
+			if (ready){
+				//socket.io
+			}
+			return ready;
 		}
 	}
 
