052113aeb1ffe9d0d16ef40723eaa26251bd71f3
HLA-systeem <HLA-systeem@users.noreply.github.com>
2016-04-07 16:04:49 +0200

    hover color changed

--- vuejs/main.js
+++ vuejs/main.js
@@ -5,6 +5,7 @@ function init() {
     var tds = document.getElementsByTagName('td');
     var tdWidth = tds[0].clientWidth;
     for (i = 0; i < tds.length; i++) {
-        tds[i].style.height = tdWidth + 'px';
+        tds[i].style.height = tdWidth + 'px';	
     }
 }
+
